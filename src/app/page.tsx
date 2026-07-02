8e046a137dc1f36da229243c8c58e2d820bbc48a
Arshdeep <arshjbdarsh@gmail.com>
2026-07-02 20:31:02 +0530

    add cipherdrop + larpring, point links to arshnah.in, cleaner work separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         {projects.map((p) => (
           <li key={p.name}>
             <a href={p.url} target="_blank" rel="noopener noreferrer">{p.name}</a>{" "}
-            ({p.year}) &middot; {p.desc}
+            ({p.year}) &ndash; {p.desc}
           </li>
         ))}
       </ul>
